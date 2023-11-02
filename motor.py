2b3e9963b2686062eca35c21fe19bc2ce54775b7
il.ydna.456 <jhouandy456@gmail.com>
2023-11-02 15:44:33 -0400

    made motor into a class

--- motor.py
+++ motor.py
@@ -1,43 +1,49 @@
 import serial
 import time
+class Motors:
 
-def get_user_input():
-    res = input()
-    return res    
+    def __init__(self, start_pos = None):
+        self.start_pos = start_pos
 
-def create_path(start_pos, end_pos):
-    path = []
-    moveX = ord(end_pos[0]) - ord(start_pos[0])
-    moveY = int(end_pos[1:]) - int(start_pos[1:])
-    while moveX < 0:
-        path.append("up")
-        moveX += 1
-    while moveX > 0:
-        path.append("down")
-        moveX -= 1
+    def get_user_input():
+        res = input()
+        return res    
 
-    while moveY < 0:
-        path.append("left")
-        moveY += 1
-    while moveY > 0:
-        path.append("right")
-        moveY -= 1
-    return path
+    def create_path(self, end_pos):
+        start_pos = self.start_pos
+        path = []
+        moveX = ord(end_pos[0]) - ord(start_pos[0])
+        moveY = int(end_pos[1:]) - int(start_pos[1:])
+        while moveX < 0:
+            path.append("U")
+            moveX += 1
+        while moveX > 0:
+            path.append("D")
+            moveX -= 1
 
-
-def send_motor_instruction(direction):
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser.reset_input_buffer()
-
-    input_str = bytes(direction + "\n")
-    ser.write(input_str)
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    time.sleep(1)
-
-def move_along_path(path):
-    for move in path:
-        send_motor_instruction(move)
+        while moveY < 0:
+            path.append("L")
+            moveY += 1
+        while moveY > 0:
+            path.append("R")
+            moveY -= 1
+        return path
 
 
-print(create_path("A1", "C9"))
+    def send_motor_instruction(self, direction):
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        ser.reset_input_buffer()
+
+        input_str = bytes(direction + "\n")
+        ser.write(input_str)
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
+
+    def move_along_path(self, path):
+        for move in path:
+            send_motor_instruction(move)
+
+
+motors = Motors("A1")
+print(motors.create_path("C9"))
